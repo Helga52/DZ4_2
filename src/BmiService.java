9378e7de6cc517297367a692ac77261df5c4ac3d
Olga <helga_ser@mail.ru>
2023-08-01 22:20:58 +0300

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
     public int calculate(int weight, float height) {
-        return (int) (weight / Math.pow(height,2));
+        return (int) (weight / Math.pow(height, 2));
     }
 }
